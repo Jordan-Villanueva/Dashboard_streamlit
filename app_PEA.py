ce15bd3d78bc4d03e46002f46ce73f013c64ed2c
Singriver <54257913+Jordan-Villanueva@users.noreply.github.com>
2024-01-25 00:29:06 +0100

    Update app_PEA.py

--- app_PEA.py
+++ app_PEA.py
@@ -74,11 +74,16 @@ gdf['NOM_ENT'][4] = 'Coahuila'
 gdf['NOM_ENT'][15] = 'Michoacán'
 gdf['NOM_ENT'][29] = 'Veracruz'
 
+#CODIGO NUEVO A PARTIR DE AQUI (O REEMPLAZAR POR ANTERIOR)
+
+# Calcular la población total
+filtered_data=filtered_data.groupby('Entidad_Federativa').sum()
 # Supongamos que la columna 'Entidad_Federativa' es la clave
 merged_data = gdf.merge(filtered_data, left_on='NOM_ENT', right_on='Entidad_Federativa', how='left')
 
+
 # Crear una nueva columna con la información deseada para el tooltip
-merged_data['Tooltip'] = merged_data.apply(lambda row: f"{row['NOM_ENT']}: {row['Poblacion_Economicamente_Activa']}", axis=1)
+merged_data['Tooltip'] = merged_data.apply(lambda row: f"{row['NOM_ENT']}: {row['Poblacion_Economica_Activa_Total']}", axis=1)
 
 m = folium.Map(location=[23.6260333, -102.5375005], tiles='OpenStreetMap', name='Light Map', zoom_start=6, attr="My Data attribution")
 
@@ -87,14 +92,14 @@ folium.Choropleth(
     geo_data=merged_data,
     name="choropleth",
     data=merged_data,
-    columns=["NOM_ENT", "Poblacion_Economicamente_Activa"],
+    columns=["NOM_ENT", "Poblacion_Economica_Activa_Total"],
     key_on="feature.properties.NOM_ENT",
     fill_color="YlOrRd",
     fill_opacity=0.7,
     line_opacity=0.1,
-    legend_name='Poblacion_Economicamente_Activa',
+    legend_name='Poblacion_Economica_Activa_Total',
     highlight=True,  # Para resaltar las entidades al pasar el cursor
-    tooltip=folium.features.GeoJsonTooltip(fields=['Tooltip'], labels=False)
+    tooltip=folium.GeoJsonTooltip(fields=['Tooltip'], aliases=['Población Total'], localize=True, sticky=False)
 ).add_to(m)
 
 # Añadir el control de capas
@@ -102,7 +107,6 @@ folium.LayerControl().add_to(m)
 
 # Desplegar el mapa
 folium_static(m, width=1600, height=950)
-
 
 # Add citation
 st.markdown("Datos obtenidos de [Datos Gubernamentales de México](https://datos.gob.mx/busca/api/3/action/package_search?q=BUSQUEDA) y [Datos CONABIO](http://geoportal.conabio.gob.mx/metadatos/doc/html/dest2019gw.html)")
