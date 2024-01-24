ad7a1ebbdf3d0609fd3e83dd61eb2b2b125aea8a
Singriver <54257913+Jordan-Villanueva@users.noreply.github.com>
2024-01-24 02:53:24 +0100

    Update app_PEA.py

--- app_PEA.py
+++ app_PEA.py
@@ -3,27 +3,28 @@ import pandas as pd
 import plotly.express as px
 from streamlit_extras.mention import mention
 
-#set the whole page
+# Establecer la configuración de la página
 st.set_page_config(layout="wide")
-# Load data
+
+# Cargar datos
 url = 'https://raw.githubusercontent.com/Jordan-Villanueva/Dashboard_Veredis/main/Tasa_de_Desocupacion.csv'
 data = pd.read_csv(url, encoding='latin-1')
 data = data[(data['Entidad_Federativa'] != 'Nacional')].reset_index(drop=True)
 data = data.drop(columns=['Unnamed: 7', 'Unnamed: 8'])
 
-# Unique years and trimesters
+# Años y trimestres únicos
 unique_years = data['Periodo'].unique()
 
-# App layout
+# Diseño de la aplicación
 st.title("Población Económica Activa en México")
 
-# Year dropdown
+# Dropdown para el año
 selected_year = st.selectbox("Seleccionar Año:", unique_years, index=len(unique_years)-1)
 
-# Add vertical separation
+# Separación vertical
 st.markdown("<br>", unsafe_allow_html=True)
 
-# Trimester dropdown
+# Dropdown para el trimestre
 if selected_year == 2023:
     trimester_options = [1, 2]
 else:
@@ -31,17 +32,13 @@ else:
 
 selected_trimester = st.selectbox("Seleccionar Trimestre:", trimester_options, index=len(trimester_options)-1)
 
-# Add vertical separation
-st.markdown("<br>", unsafe_allow_html=True)
-st.markdown("<br>", unsafe_allow_html=True)
-st.markdown("<br>", unsafe_allow_html=True)
+# Separación vertical
 st.markdown("<br>", unsafe_allow_html=True)
 
-
-# Filter data
+# Filtrar datos
 filtered_data = data[(data['Periodo'] == selected_year) & (data['Trimestre'] == selected_trimester)]
 
-# Plot
+# Gráfico
 fig = px.bar(
     filtered_data,
     x='Entidad_Federativa',
@@ -52,13 +49,17 @@ fig = px.bar(
     color_discrete_sequence=['steelblue', 'magenta'],
     title=f'Población Económica Activa en {selected_year} - Trimestre {selected_trimester}'
 )
-# Customize axis titles
+
+# Personalizar títulos de los ejes
 fig.update_xaxes(title_text='Entidad Federativa')
 fig.update_yaxes(title_text='Población (millones de habitantes)')
 fig.update_xaxes(tickangle=-60)
 
-# Use st.plotly_chart with custom width
+# Usar st.plotly_chart con ancho personalizado
 st.plotly_chart(fig, use_container_width=True)
 
-mention(label = f"[{Fuente}](https://datos.gob.mx/busca/api/3/action/package_search?q=BUSQUEDA)")
+# Definir la fuente
+Fuente = "Datos Gubernamentales de México"
 
+# Agregar la mención al final del sitio web
+mention(label=f"{Fuente}", url="https://datos.gob.mx/busca/api/3/action/package_search?q=BUSQUEDA")
